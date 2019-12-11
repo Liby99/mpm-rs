feb1b195599d4e7b8da2e071e0b2c99cdb263deb
Ziyang Li <liby99@seas.upenn.edu>
2019-12-11 11:19:16 -0500

    Add single particle example

---
+++ src/bin/single_par.rs
@@ -0,0 +1,23 @@
+use mpm_rs::*;
+
+fn main() {
+
+  // Before all, create the directory
+  let outdir = "result/single_par_out".to_string();
+  if let Err(err) = std::fs::create_dir(&outdir) { panic!(err); }
+
+  // First construct the world, size 1 * 1 * 1
+  let mut world = World::new(0.01, Vector3u::new(100, 100, 100));
+
+  // Build the boundaries
+  put_boundary(&mut world, 0.03);
+
+  // Create a ball with only one particle
+  put_ball(&mut world, Vector3f::new(0.5, 0.8, 0.5), 0.0001, 1, 1.0);
+
+  // Then build the driver
+  let mut driver = Driver::new(world, 0.03333);
+
+  // Finally run the driver
+  if let Err(err) = driver.run(outdir, 500) { panic!(err); }
+}
