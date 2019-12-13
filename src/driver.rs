7fd361228c113ba23ec7d44ab3949b07242c84a0
Ziyang Li <liby99@seas.upenn.edu>
2019-12-13 16:22:38 -0500

    Minor driver fix

--- src/driver.rs
+++ src/driver.rs
@@ -13,20 +13,20 @@ impl Driver {
   }
 
   pub fn run(&mut self, outdir: String, num_steps: usize) -> std::io::Result<()> {
-    // First dump the default frame
+    // First dump the initial frame to be frame 0
     let filename = format!("{}/{}.poly", outdir, 0);
     self.world.dump(filename)?;
 
-    // Then
+    // Then dump the `num_steps` frame
     let mut pb = ProgressBar::new(num_steps as u64);
-    for frame in 0..num_steps {
+    for frame in 1..num_steps {
       pb.inc();
 
       // First step the world forward
       self.world.step(self.dt);
 
       // Then get the filename and dump the particles
-      let filename = format!("{}/{}.poly", outdir, frame + 1);
+      let filename = format!("{}/{}.poly", outdir, frame);
       self.world.dump(filename)?;
     }
     pb.finish_print("Done");
